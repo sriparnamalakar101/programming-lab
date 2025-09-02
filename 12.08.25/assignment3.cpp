a9f3fbf34603635277589e048e7a7faa18b880dc
Sriparna_333 <sriparna.malakar2020@gmail.com>
2025-09-02 22:17:20 +0530

    Add files via upload

---
+++ 12.08.25/assignment3.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+    int main(){
+    	float n1,n2,diff;
+    	printf("Enter first number:");
+    	scanf("%f",&n1);
+    	printf("Enter seconmd number:");
+    	scanf("%f",&n2);
+    	diff= n1-n2;
+    	printf("%f\n", diff);
+    	return 0;
+	}
