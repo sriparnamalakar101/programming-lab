666fc5125c5c0083aa4c60167e140278c28d8cad
Sriparna_333 <sriparna.malakar2020@gmail.com>
2025-09-02 22:04:12 +0530

    Add files via upload

---
+++ 12.08.25/assignment2.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+  int main(){
+  	float n1,n2,sum;
+  	printf("Enter first number: ");
+  	scanf("%f",&n1);
+  	printf("Enter second number: ");
+  	scanf("%f",&n2);
+  	sum=n1+n2;
+  	printf("%f\n", sum);
+  	return 0;
+  }
