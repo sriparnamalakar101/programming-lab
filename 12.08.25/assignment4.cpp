e787eb71bb8e7c6c6245aed16b9e94dd0534d1b6
Sriparna_333 <sriparna.malakar2020@gmail.com>
2025-09-02 22:47:07 +0530

    Add files via upload

---
+++ 12.08.25/assignment4.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+    int main(){
+    	int n1;
+    	printf("Enter a number whose table you want:");
+    	scanf("%d ",&n1);
+    	
+    	printf("%d\n",n1*1);
+    	printf("%d\n",n1*2);
+    	printf("%d\n",n1*3);
+    	printf("%d\n",n1*4);
+    	printf("%d\n",n1*5);
+    	printf("%d\n",n1*6);
+    	printf("%d\n",n1*7);
+    	printf("%d\n",n1*8);
+    	printf("%d\n",n1*9);
+    	printf("%d\n",n1*10);
+    	return 0;
+	}
